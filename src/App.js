56159c327fddd7fbbb3b19c6b6efc4b4b8f2fe6b
glacks06(mac) <a91125149@gmail.com>
2025-09-16 10:56:48 +0900

    App.js에 map함수 테스트용 코드 삽입

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import './css/App.css';
 import {Header, Dive_lanking, About} from './components'
 import { Routes, Route } from 'react-router-dom';
 
+let arr = ['호랑이', '코끼리', '독수리'];
+let brr = arr.map((value, key) => <li key={key}>{value}</li>)
+console.log(brr);
+// map함수 테스트중
+
+
 function App() {
   return (
     <div className="App">
