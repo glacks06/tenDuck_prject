3e321ecb7b101e294d743f1d8ffe115a682655af
glacks06(mac) <a91125149@gmail.com>
2025-09-17 13:42:40 +0900

    애니메이션 분류에 따른 arr 선언

---
+++ src/assets/anims_list/anims.js
@@ -0,0 +1,29 @@
+anims_100m = [
+    {name: "원피스"}
+];
+
+anims_200m = [
+    {name: "원피스"}
+];
+
+anims_500m = [
+    {name: "원피스"}
+];
+
+anims_1000m = [
+    {name: "원피스"}
+];
+
+anims_1500m = [
+    {name: "원피스"}
+];
+
+anims_2000m = [
+    {name: "원피스"}
+];
+
+anims_3000m = [
+    {name: "원피스"}
+];
+
+anims = [anims_100m, anims_200m, anims_500m, anims_1000m, anims_1500m, anims_2000m, anims_3000m];
