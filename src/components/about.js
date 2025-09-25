b8eca4afb5eb2998298323ecaecb6a5ec01e0b51
glacks06(mac) <a91125149@gmail.com>
2025-09-25 13:41:45 +0900

    barrier 내용 일부 수정

--- src/components/about.js
+++ src/components/about.js
@@ -20,7 +20,7 @@ function About(){
                 </p>
                 <p>
                     <h1>Version</h1>
-                    v0.0.0
+                    v0.0.1
                 </p>
             </div>
         </div>
