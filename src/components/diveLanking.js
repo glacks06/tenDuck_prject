7b326a4ab82803da738339b6cb4ee7dcf223c38f
glacks06(mac) <a91125149@gmail.com>
2025-09-26 00:56:50 +0900

    편의기능에서 특정 수심 클릭 시 해당 수심으로 이동되는 기능 추가

--- src/components/diveLanking.js
+++ src/components/diveLanking.js
@@ -1,44 +1,54 @@
 import styles from '../modules/_dive_lanking.module.scss'
 import Anims from './animsArticle';
 import anims from '../assets/anims_list/anims';
+import { useRef, useEffect } from 'react';
+import depth_inform from '../assets/info/depth_info'
 
-// Anims에 anims_list 전달
+
 
 function DiveLanking(){
+    const buttonRef = useRef([]);
+    const buttons = ['100m', '150m', '200m', '500m', '1000m', '1500m']
+
+    const targetRef = useRef([]);
+    const targets = ['100m', '150m', '200m', '500m', '1000m', '1500m'];
+
+    useEffect(()=>{
+        const buttonElement = buttonRef.current;
+        const targetElement = targetRef.current;
+        console.log(targetElement);
+        
+        for(let i = 0; i < buttons.length; i += 1){
+            buttonElement[i].addEventListener('click', () => {
+                targetElement[i].scrollIntoView({ behavior: 'smooth', block: 'end' });
+            });
+
+        }
+    },[]);
+
     return(
         <div className={styles.base}>
-            <img src={`${process.env.PUBLIC_URL}/imgs/logo.png`} alt='logo' className={styles.remote} />
-            <section>
-                <Anims anims_for_display={anims[0]}/>
-                <p className={styles.depth_num}>100m</p>
-                <div className={styles.depth_inform}>완전 메이저. 일반인한테 추천해도 이상하게 보이지 않음</div>
-            </section>
-            <section>
-                <Anims anims_for_display={anims[1]}/>
-                <p className={styles.depth_num}>150m</p>
-                <div className={styles.depth_inform}>메이저라고 하기엔 아주 살짝 애매. 패션씹덕이거나 입덕부정기일 가능성이 있음</div>
-            </section>
-            <section>
-                <Anims anims_for_display={anims[2]}/>
-                <p className={styles.depth_num}>200m</p>
-                <div className={styles.depth_inform}>진입장벽은 낮은데 인지도가 없는 작품</div>
-                
-            </section>
-            <section>
-                <Anims anims_for_display={anims[3]}/>
-                <p className={styles.depth_num}>500m</p>
-                <div className={styles.depth_inform}>약간의 진입장벽. 일반인이 애니를 보았을 때 표정이 썩기 시작하는 구간?</div>
-            </section>
-            <section>
-                <Anims anims_for_display={anims[4]}/>
-                <p className={styles.depth_num}>1000m</p>
-                <div className={styles.depth_inform}>진입어려움.</div>
-            </section>
-            <section>
-                <Anims anims_for_display={anims[5]}/>
-                <p className={styles.depth_num}>1500m</p>
-                <div className={styles.depth_inform}>쉽지않은 주제, 심연인가 싶은 작품</div>
-            </section>
+
+            <div className={styles.remoteBox}>
+                {
+                    buttons.map((items, index) => (
+                        <p key={items} ref={el => buttonRef.current[index] = el}>
+                            {items}
+                        </p>
+                    ))
+                }
+            </div>
+            <img src={`${process.env.PUBLIC_URL}/imgs/logo.png`} alt='logo' className={styles.remoteButton} />
+
+            {
+                targets.map((item, index) => (
+                    <section key={item} ref={el => targetRef.current[index] = el}>
+                        <Anims anims_for_display={anims[index]}/>
+                        <p className={styles.depth_num}>{item}</p>
+                        <div className={styles.depth_inform}>{depth_inform[index]}</div>
+                    </section>
+                ))
+            }
         </div>
     );
 };
---
+++ src/side_effects/scrollToDepth.js
@@ -0,0 +1,20 @@
+// import React, { useRef, useEffect } from 'react';
+
+// function scrollToDepth(buttonElementClassName, targetElementClassName) {
+//     const button = useRef(null);
+//     const target = useRef(null);
+
+//     useEffect(()=>{
+//         const buttonElement = button.current;
+//         const targetElement = target.current;
+
+//         buttonElement.addEventListener('click', () => {
+//             targetElement.scrollIntoView({ behavior: 'smooth', block: 'end' });
+//         });
+//     },[]);
+
+    
+
+// }
+
+// export default scrollToDepth;
